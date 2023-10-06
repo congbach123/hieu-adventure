da6f3ffa5dc23ab3abefc55acc116fd5f9a14b61
Baccon <bachcong.hn@gmail.com>
2023-10-06 16:22:24 +0700

    commit 4rd

---
+++ src/main/GamePanel.java
@@ -0,0 +1,10 @@
+package main;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    final int originalTileSize = 32;
+    final int scale = 2;
+
+    final int tileSize =asdadas;
+}
