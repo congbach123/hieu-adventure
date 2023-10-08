460551c1155e86ae5e6694c455256b49cf4c33af
Baccon <bachcong.hn@gmail.com>
2023-10-08 20:21:53 +0700

    xong vid 3

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,7 +9,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 32;
     final int scale = 2;
 
-    final int tileSize = originalTileSize * scale; // 64*64 tile
+    public final int tileSize = originalTileSize * scale; // 64*64 tile
     final int maxScreenCol = 16;
     final int maxSCreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 1024 px
@@ -16,13 +18,8 @@ public class GamePanel extends JPanel implements Runnable{
     int FPS = 60;
 
     KeyHandler keyH = new KeyHandler();
-
     Thread gameThread;
-
-    //set player default pos
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+    Player player = new Player(this, keyH);
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
@@ -98,29 +95,13 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
     public void update(){
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-
-        }
-        else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX,playerY, tileSize, tileSize);
-
+        player.draw(g2);
         g2.dispose();
     }
 }
